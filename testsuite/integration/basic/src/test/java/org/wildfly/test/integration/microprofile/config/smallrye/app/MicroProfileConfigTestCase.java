1be35a31436bd14c7c365f8346680ae149c62e8b
Jan Stourac <jstourac@redhat.com>
2018-07-18 21:03:24 +0200

    [WFLY-10522] test extension to check various data types and simple priority checks.
    - various data types are check that they work as expected in regards to default values,
      overridden values and empty values (no default)
    - simple smoke checks for priority when properties are loaded from different sources

--- testsuite/integration/basic/src/test/java/org/wildfly/test/integration/microprofile/config/smallrye/app/MicroProfileConfigTestCase.java
+++ testsuite/integration/basic/src/test/java/org/wildfly/test/integration/microprofile/config/smallrye/app/MicroProfileConfigTestCase.java
@@ -25,8 +25,13 @@ package org.wildfly.test.integration.microprofile.config.smallrye.app;
 import static org.wildfly.test.integration.microprofile.config.smallrye.AssertUtils.assertTextContainsProperty;
 import static org.wildfly.test.integration.microprofile.config.smallrye.HttpUtils.getContent;
 
+import java.io.File;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Optional;
+import java.util.Set;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -40,6 +45,7 @@ import org.jboss.as.arquillian.api.ServerSetup;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
@@ -48,6 +54,7 @@ import org.wildfly.test.integration.microprofile.config.smallrye.SubsystemConfig
 
 /**
  * @author <a href="http://jmesnil.net/">Jeff Mesnil</a> (c) 2017 Red Hat inc.
+ * @author Jan Stourac <jstourac@redhat.com>
  */
 @RunWith(Arquillian.class)
 @RunAsClient
@@ -57,25 +64,287 @@ public class MicroProfileConfigTestCase {
     @Deployment
     public static Archive<?> deploy() {
         WebArchive war = ShrinkWrap.create(WebArchive.class, "MicroProfileConfigTestCase.war")
-                .addClasses(TestApplication.class, TestApplication.Resource.class)
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+                .addClasses(TestApplication.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsManifestResource(MicroProfileConfigTestCase.class.getPackage(),
+                        "microprofile-config.properties", "microprofile-config.properties");
+
+        war.as(ZipExporter.class).exportTo(new File("/tmp", war.getName()), true);
         return war;
     }
 
     @ArquillianResource
     private URL url;
 
+    // TODO also check that server log contains no error...?
+
+    private final String appContext = "microprofile";
+
+    /**
+     * Check that we get default values for properties except for one, which should have value loaded from the
+     * subsystem. There is also checked that property form META-INF file and also some System Property is loaded.
+     *
+     * @throws Exception
+     */
     @Test
     public void testGetWithConfigProperties() throws Exception {
         try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
-            HttpResponse response = client.execute(new HttpGet(url + "microprofile/test"));
+            HttpResponse response = client.execute(new HttpGet(url + appContext + TestApplication.APP_PATH));
             Assert.assertEquals(200, response.getStatusLine().getStatusCode());
             String text = getContent(response);
+
             assertTextContainsProperty(text, "my.prop.never.defined", Optional.empty().toString());
             assertTextContainsProperty(text, "my.prop", "BAR");
             assertTextContainsProperty(text, "my.other.prop", false);
-            assertTextContainsProperty(text, "optional.injected.prop.that.is.not.configured", Optional.empty().toString());
-            assertTextContainsProperty(text, SubsystemConfigSourceTask.MY_PROP_FROM_SUBSYSTEM_PROP_NAME, SubsystemConfigSourceTask.MY_PROP_FROM_SUBSYSTEM_PROP_VALUE);
+            assertTextContainsProperty(text, "optional.injected.prop.that.is.not.configured",
+                    Optional.empty().toString());
+
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.MY_PROP_FROM_SUBSYSTEM_PROP_NAME,
+                    SubsystemConfigSourceTask.MY_PROP_FROM_SUBSYSTEM_PROP_VALUE);
+            // TODO - this is weird... server shows different path to what is retrieved here:
+            // - server: /home/jstourac/workspace/wildfly/testsuite/integration/basic/target/wildfly
+            // - this:   /home/jstourac/workspace/wildfly/testsuite/integration/basic/../../../build/target/wildfly-14.0.0.Beta1-SNAPSHOT
+            assertTextContainsProperty(text, "jboss.home", System.getProperty("jboss.home"));
+            assertTextContainsProperty(text, "JBOSS_HOME", System.getenv("JBOSS_HOME"));
+        }
+    }
+
+    /**
+     * Check boolean/Boolean type is correctly handled in regards of the default values, no default values and if it is
+     * overridden.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetBooleanProperties() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            HttpResponse response = client.execute(new HttpGet(url + appContext + TestApplication.BOOLEAN_APP_PATH));
+            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+            String text = getContent(response);
+
+            assertTextContainsProperty(text, "boolTrue", true);
+            assertTextContainsProperty(text, "bool1", true);
+            assertTextContainsProperty(text, "boolYes", true);
+            assertTextContainsProperty(text, "boolY", true);
+            assertTextContainsProperty(text, "boolOn", true);
+
+            assertTextContainsProperty(text, "boolNoDefault", false);
+            assertTextContainsProperty(text, "boolDefault", true);
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.BOOL_OVERRIDDEN_PROP_NAME, true);
+
+            assertTextContainsProperty(text, "booleanNoDefault", false);
+            assertTextContainsProperty(text, "booleanDefault", true);
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.BOOLEAN_OVERRIDDEN_PROP_NAME, true);
+        }
+    }
+
+    /**
+     * Check int/Integer type is correctly handled in regards of the default values, no default values and if it is
+     * overridden.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetIntegerProperties() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            HttpResponse response = client.execute(new HttpGet(url + appContext + TestApplication.INTEGER_APP_PATH));
+            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+            String text = getContent(response);
+
+            assertTextContainsProperty(text, "intNoDefault", 0); // TODO - does it make sense to check default values?
+            assertTextContainsProperty(text, "intDefault", -42);
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.INT_OVERRIDDEN_PROP_NAME,
+                    SubsystemConfigSourceTask.INTEGER_OVERRIDDEN_PROP_VALUE);
+
+            assertTextContainsProperty(text, "integerNoDefault", "null");
+            assertTextContainsProperty(text, "integerDefault", -42);
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.INTEGER_OVERRIDDEN_PROP_NAME,
+                    SubsystemConfigSourceTask.INTEGER_OVERRIDDEN_PROP_VALUE);
+
+            assertTextContainsProperty(text, "intBadValue", 0);
+            assertTextContainsProperty(text, "integerBadValue", "null");
+        }
+    }
+
+    /**
+     * Check long/Long type is correctly handled in regards of the default values, no default values and if it is
+     * overridden.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetLongProperties() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            HttpResponse response = client.execute(new HttpGet(url + appContext + TestApplication.LONG_APP_PATH));
+            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+            String text = getContent(response);
+
+            assertTextContainsProperty(text, "longNoDefault", 0); // TODO - does it make sense to check default values?
+            assertTextContainsProperty(text, "longDefault", -42);
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.LONG_OVERRIDDEN_PROP_NAME, SubsystemConfigSourceTask.LONG_OVERRIDDEN_PROP_VALUE);
+
+            assertTextContainsProperty(text, "longClassNoDefault", "null");
+            assertTextContainsProperty(text, "longClassDefault", -42);
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.LONG_CLASS_OVERRIDDEN_PROP_NAME, SubsystemConfigSourceTask.LONG_OVERRIDDEN_PROP_VALUE);
+
+            assertTextContainsProperty(text, "longBadValue", 0);
+            assertTextContainsProperty(text, "longClassBadValue", "null");
+        }
+    }
+
+    /**
+     * Check float/Float type is correctly handled in regards of the default values, no default values and if it is
+     * overridden.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetFloatProperties() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            HttpResponse response = client.execute(new HttpGet(url + appContext + TestApplication.FLOAT_APP_PATH));
+            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+            String text = getContent(response);
+
+            assertTextContainsProperty(text, "floatNoDefault", 0.0f); // TODO - does it make sense to check default values?
+            assertTextContainsProperty(text, "floatDefault", -3.14);
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.FLOAT_OVERRIDDEN_PROP_NAME, SubsystemConfigSourceTask.FLOAT_OVERRIDDEN_PROP_VALUE);
+
+            assertTextContainsProperty(text, "floatClassNoDefault", "null");
+            assertTextContainsProperty(text, "floatClassDefault", Float.valueOf("-3.14e10"));
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.FLOAT_CLASS_OVERRIDDEN_PROP_NAME, SubsystemConfigSourceTask.FLOAT_OVERRIDDEN_PROP_VALUE);
+
+            assertTextContainsProperty(text, "floatBadValue", 0.0f);
+            assertTextContainsProperty(text, "floatClassBadValue", "null");
+        }
+    }
+
+    /**
+     * Check double/Double type is correctly handled in regards of the default values, no default values and if it is
+     * overridden.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetDoubleProperties() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            HttpResponse response = client.execute(new HttpGet(url + appContext + TestApplication.DOUBLE_APP_PATH));
+            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+            String text = getContent(response);
+
+            assertTextContainsProperty(text, "doubleNoDefault", 0.0d); // TODO - does it make sense to check default values?
+            assertTextContainsProperty(text, "doubleDefault", -3.14);
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.DOUBLE_OVERRIDDEN_PROP_NAME, SubsystemConfigSourceTask.DOUBLE_OVERRIDDEN_PROP_VALUE);
+
+            assertTextContainsProperty(text, "doubleClassNoDefault", "null");
+            assertTextContainsProperty(text, "doubleClassDefault", Double.valueOf("-3.14e10"));
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.DOUBLE_CLASS_OVERRIDDEN_PROP_NAME, SubsystemConfigSourceTask.DOUBLE_OVERRIDDEN_PROP_VALUE);
+
+            assertTextContainsProperty(text, "doubleBadValue", 0.0d);
+            assertTextContainsProperty(text, "doubleClassBadValue", "null");
+        }
+    }
+
+    /**
+     * Check String array, List and Set properties are correctly handled in regards of the default values.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetWithArraySetListDefaultProperties() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            HttpResponse response = client.execute(new HttpGet(url + appContext + TestApplication.ARRAY_SET_LIST_DEFAULT_APP_PATH));
+            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+            String text = getContent(response);
+
+            LinkedList<String> petsList = new LinkedList<>();
+            petsList.add("cat");
+            petsList.add("lama,yokohama");
+
+            Set<String> petsSet = new HashSet<>();
+            petsSet.add("dog");
+            petsSet.add("mouse,house");
+
+            assertTextContainsProperty(text, "myPets as String array", Arrays.toString(new String[]{"horse","monkey,donkey"}));
+            assertTextContainsProperty(text, "myPets as String list", petsList);
+            assertTextContainsProperty(text, "myPets as String set", petsSet); // TODO - not sure whether this is safe as Set doesn't assure order?
+        }
+    }
+
+    /**
+     * Check String array, List and Set properties are correctly handled if their default values are overridden.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetWithArraySetListOverriddenProperties() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            HttpResponse response = client.execute(new HttpGet(url + appContext + TestApplication.ARRAY_SET_LIST_OVERRIDE_APP_PATH));
+            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+            String text = getContent(response);
+
+            LinkedList<String> petsList = new LinkedList<>();
+            petsList.add("donkey");
+            petsList.add("shrek,fiona");
+
+            Set<String> petsSet = new HashSet<>();
+            petsSet.add("donkey");
+            petsSet.add("shrek,fiona");
+
+            assertTextContainsProperty(text, "myPetsOverridden as String array", Arrays.toString(new String[] {"donkey", "shrek,fiona"}));
+            assertTextContainsProperty(text, "myPetsOverridden as String list", petsList);
+            assertTextContainsProperty(text, "myPetsOverridden as String set", petsSet); // TODO - not sure whether this is safe as Set doesn't assure order?
+//            Assert.assertTrue(text.contains("myPetsOverridden as String set = [donkey,shrek]") || text.contains("myPetsOverridden as String set = [shrek,donkey]"));
+        }
+    }
+
+    /**
+     * Check String array, List and Set properties are correctly handled if they are not set and have no default values.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetWithArraySetListNoDefaultProperties() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            HttpResponse response = client.execute(new HttpGet(url + appContext + TestApplication.ARRAY_SET_LIST_NO_DEF_APP_PATH));
+            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+            String text = getContent(response);
+
+            assertTextContainsProperty(text, "myPetsNonDef as String array", Arrays.toString(new String[] {}));
+            assertTextContainsProperty(text, "myPetsNonDef as String list", new LinkedList<>());
+            assertTextContainsProperty(text, "myPetsNonDef as String set", new HashSet<>());
+        }
+    }
+
+    /**
+     * Checks that properties with same names are loaded based on their priorities defined by their sources.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testPriorityOrderingProperties() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            HttpResponse response = client.execute(new HttpGet(url + appContext + TestApplication.PRIORITY_APP_PATH));
+            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+            String text = getContent(response);
+
+            // Values from META-INF
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.PROPERTIES_PROP_NAME0, "Value prop0 from META-INF/microprofile-config.properties");
+            // TODO - get env property ???
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.PROPERTIES_PROP_NAME1, SubsystemConfigSourceTask.PROP1_VALUE);
+            // Value from defined system property in subsystem overrided meta-inf
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.PROPERTIES_PROP_NAME2, SubsystemConfigSourceTask.PROP2_VALUE);
+
+            // fileProperty has ordinal value 100, same as default for META-INF properties, thus system property should override this
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.PROPERTIES_PROP_NAME3, SubsystemConfigSourceTask.PROP3_VALUE);
+
+            // TODO - this check fails but not sure why, yet - have to go now, will investigate tomorrow...
+            // dir property should override all in this case
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.PROPERTIES_PROP_NAME4, "priority.prop.4 value loaded via directory config-source fileProperty4");
+
+            // not defined anywhere...
+            assertTextContainsProperty(text, SubsystemConfigSourceTask.PROPERTIES_PROP_NAME5, "Custom file property not defined!");
+
+            // TODO we should add also test for properties with same priority but from different sources
         }
     }
 }
